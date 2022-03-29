ec469a28c19793684a2b7ebb7d86ef1b6c0f8e2c
Latif <latif@robomotion.io>
2022-03-29 11:46:33 +0300

    get folder public link

--- public_links.go
+++ public_links.go
@@ -7,7 +7,7 @@ import (
 	"strconv"
 )
 
-// CreateFolder; https://docs.pcloud.com/methods/folder/createfolder.html
+// GetFilePubLink; https://docs.pcloud.com/methods/public_links/getfilepublink.html
 func (c *PCloudClient) GetFilePubLink(path string, fileID int) (string, error) {
 	values := url.Values{
 		"auth": {*c.Auth},
@@ -22,6 +22,41 @@ func (c *PCloudClient) GetFilePubLink(path string, fileID int) (string, error) {
 		return "", errors.New("bad params")
 	}
 	resp, err := c.Client.Get(urlBuilder("getfilepublink", values))
+
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	result := struct {
+		Link   string `json:"link"`
+		Result int    `json:"result"`
+		Error  string `json:"error"`
+	}{}
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return "", err
+	}
+	if result.Result > 0 {
+		return "", errors.New(result.Error)
+	}
+	return result.Link, nil
+}
+
+// GetFolderPubLink; https://docs.pcloud.com/methods/public_links/getfolderpublink.html
+func (c *PCloudClient) GetFolderPubLink(path string, fileID int) (string, error) {
+	values := url.Values{
+		"auth": {*c.Auth},
+	}
+
+	switch {
+	case path != "":
+		values.Add("path", path)
+	case fileID >= 0:
+		values.Add("fileid", strconv.Itoa(fileID))
+	default:
+		return "", errors.New("bad params")
+	}
+	resp, err := c.Client.Get(urlBuilder("getfolderpublink", values))
 
 	if err != nil {
 		return "", err
