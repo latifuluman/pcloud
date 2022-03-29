5ee1001f3a5ac0a03c66ac2c2f01b8d486354fc9
Latif <latif@robomotion.io>
2022-03-29 11:28:21 +0300

    .

--- public_links.go
+++ public_links.go
@@ -1,14 +1,14 @@
 package pcloud
 
 import (
+	"encoding/json"
 	"errors"
-	"net/http"
 	"net/url"
 	"strconv"
 )
 
 // CreateFolder; https://docs.pcloud.com/methods/folder/createfolder.html
-func (c *PCloudClient) GetFilePubLink(path string, fileID int) (*http.Response, error) {
+func (c *PCloudClient) GetFilePubLink(path string, fileID int) (string, error) {
 	values := url.Values{
 		"auth": {*c.Auth},
 	}
@@ -19,9 +19,25 @@ func (c *PCloudClient) GetFilePubLink(path string, fileID int) (*http.Response, 
 	case fileID >= 0:
 		values.Add("fileid", strconv.Itoa(fileID))
 	default:
-		return nil, errors.New("bad params")
+		return "", errors.New("bad params")
 	}
-	return c.Client.Get(urlBuilder("getfilepublink", values))
+	resp, err := c.Client.Get(urlBuilder("getfilepublink", values))
 
-	//return checkResult()
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	result := struct {
+		Link   string `json:"link"`
+		Result int    `json:"result"`
+		Error  string `json:"error"`
+	}{}
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return "", err
+	}
+	if result.Result > 0 {
+		return "", errors.New(result.Error)
+	}
+	return result.Link, nil
 }
